ac2568a2fbd1b78b7ea28af581dab711ed4d7c25
Jorge Aparicio <japaricious@gmail.com>
2016-04-29 12:43:28 -0500

    allow using ssh keys for local git operations

--- launch.py
+++ launch.py
@@ -7,6 +7,13 @@ import sys
 with open('cfg.template.toml') as f:
     template = Template(f.read())
 
+ssh_key = os.environ.get('GIT_SSH_KEY', '')
+
+if ssh_key:
+    local_git = "true"
+else:
+    local_git = "false"
+
 repos = map(lambda slug: slug.split('/'), os.environ['HOMU_REPOS'].split(' '))
 
 homu = {
@@ -15,6 +22,10 @@ homu = {
         'oauth_id': os.environ['GH_OAUTH_ID'],
         'oauth_secret': os.environ['GH_OAUTH_SECRET'],
         'webhook_secret': os.environ['GH_WEBHOOK_SECRET'],
+    },
+    'git': {
+        'local_git': 'true' if ssh_key else 'false',
+        'ssh_key': ssh_key,
     },
     'repos': repos,
     'reviewers': os.environ['HOMU_REVIEWERS'].split(' '),
